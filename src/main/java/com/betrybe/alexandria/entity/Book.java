2c2f91423b90f23bec1f13546fa4fa994692ad6c
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-03-20 13:45:14 -0300

    annotating entities

--- src/main/java/com/betrybe/alexandria/entity/Book.java
+++ src/main/java/com/betrybe/alexandria/entity/Book.java
@@ -1,10 +1,20 @@
 package com.betrybe.alexandria.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
  * The type Book.
  */
+@Entity
+@Table(name = "books")
 public class Book {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String title;
